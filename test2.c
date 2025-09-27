d4c5d5948f25b7313648710c9a87009b521f7e31
qzqgg22 <1975543030@qq.com>
2025-09-27 14:54:41 +0800

    test2.c

--- test2.c
+++ test2.c
@@ -2,16 +2,35 @@
 #include <string.h>
 
 int main() {
-    char input[20];
+    char input[50];
+    char actual_key[50];
     
     printf("Enter key (or 'Quit' to exit):\n");
 
     while (1) {
         printf("> ");
-        scanf("%s", input);
+        
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;
+        }
+        
+        int len = strlen(input);
+        if (len > 0 && input[len-1] == '\n') {
+            input[len-1] = '\0';
+        }
+        
+        if (strlen(input) == 0) {
+            continue;
+        }
         
         if (strcmp(input, "Quit") == 0) {
             break;
+        }
+        
+        strcpy(actual_key, input);
+        char *space_pos = strchr(actual_key, ' ');
+        if (space_pos != NULL) {
+            *space_pos = '\0';  
         }
         
         FILE *file = fopen("data.txt", "r");
@@ -20,7 +39,7 @@ int main() {
             continue;
         }
         
-        char line[50];
+        char line[100];
         int found = 0;
         
         while (fgets(line, sizeof(line), file) != NULL) {
@@ -28,16 +47,27 @@ int main() {
             if (colon == NULL) continue;
             
             *colon = '\0';
-            if (strcmp(input, line) == 0) {
-    char *value = colon + 1;
-    int len = strlen(value);
-    if (len > 0 && value[len-1] == '\n') {
-        value[len-1] = '\0';
-    }
-    printf("%s\n", value);  
-    found = 1;
-    break;
-}
+            
+            char *key = line;
+            while (*key == ' ') key++;
+            char *key_end = key + strlen(key) - 1;
+            while (key_end > key && isspace((unsigned char)*key_end)) {
+                *key_end = '\0';
+                key_end--;
+            }
+            
+            if (strcmp(actual_key, key) == 0) {
+                char *value = colon + 1;
+                int value_len = strlen(value);
+                if (value_len > 0 && value[value_len-1] == '\n') {
+                    value[value_len-1] = '\0';
+                }
+                
+                while (*value == ' ') value++;
+                printf("%s\n", value);  
+                found = 1;
+                break;
+            }
         }
         
         fclose(file);
